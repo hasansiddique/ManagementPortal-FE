d06280c9ff62d7e3b5600849188697b284500ee8
Bilal Ahmed Janjua <bilalahmjan@gmail.com>
2020-05-21 21:08:10 +0500

    reducer and action files for auth removed!

--- src/state/rootReducer.js
+++ src/state/rootReducer.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux-immutable';
 
-// import auth from '../views/auth/auth.reducer';
 import auth from '../views/auth/auth.store';
 import dashboard from '../views/dashboard/dashboard.reducer';
 
